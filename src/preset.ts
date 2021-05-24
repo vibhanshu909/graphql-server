b20edc098b4f7beb95f21de80609ec7174998042
vibhanshu pandey <vibhanshu909@gmail.com>
2021-05-24 18:55:45 +0530

    Grouped commands together

--- src/preset.ts
+++ src/preset.ts
@@ -1,19 +1,21 @@
 import { color, Preset } from "apply";
 import fs from "fs";
 
-Preset.setName("Graphql preset");
-Preset.editNodePackages()
-  .add("apollo-server-lambda", "^2.24.1")
-  .add("graphql", "^15.5.0");
-if (fs.existsSync("tsconfig.json")) {
-  console.log(`${color.magenta("Detected typescript project")}`);
-  Preset.extract("ts");
-} else {
-  Preset.extract("js");
-}
+Preset.setName("Graphql preset").group((preset) => {
+  preset
+    .editNodePackages()
+    .add("apollo-server-lambda", "^2.24.1")
+    .add("graphql", "^15.5.0");
+  if (fs.existsSync("tsconfig.json")) {
+    console.log(`${color.magenta("Detected typescript project")}`);
+    preset.extract("ts");
+  } else {
+    preset.extract("js");
+  }
 
-Preset.instruct(
-  `Run ${color.magenta("npm install")}, ${color.magenta(
-    "pnpm install"
-  )}, or ${color.magenta("yarn")} to install dependencies`
-);
+  preset.instruct(
+    `Run ${color.magenta("npm install")}, ${color.magenta(
+      "pnpm install"
+    )}, or ${color.magenta("yarn")} to install dependencies`
+  );
+});
